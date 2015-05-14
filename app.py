45236e2718116d81c7e81f468e832d2ef1802649
Linsir <vi5i0n@hotmail.com>
2015-05-14 23:29:40 +0800

    limit of file size

--- app.py
+++ app.py
@@ -35,24 +35,45 @@ class root(BaseHandler):
     def post(self):
         file = self.request.files['filedata'][0]
         filedata = file['body']
-        print file['content_type']
-        reload(sys)
-        sys.setdefaultencoding('utf-8')
-        try:
-            html = misaka.html(filedata)
-            buffer = BytesIO()
-            pdf = pisa.CreatePDF(html,buffer)
-            pdf = buffer.getvalue()
-            buffer.close()
-            saved_file_name = 'attachment; filename=' + file['filename'] + '.pdf'
-
-            self.set_header("Content-Type", "application/pdf")
-            self.set_header('Content-Disposition', saved_file_name)
-            self.write(pdf)
-        except Exception, e:
-            msg = "The File is not written using markdown!"
+        filename = file['filename']
+        # print file['content_type']
+        if os.path.getsize(save(filename, filedata)) >= 5120:
+            msg = "The File is bigger than 5 KB!"
             self.render("error.html",msg=msg)
+        else:
+            reload(sys)
+            sys.setdefaultencoding('utf-8')
+            try:
+                html = misaka.html(filedata)
+                buffer = BytesIO()
+                pdf = pisa.CreatePDF(html,buffer)
+                pdf = buffer.getvalue()
+                buffer.close()
+                saved_file_name = 'attachment; filename=' + file['filename'] + '.pdf'
 
+                self.set_header("Content-Type", "application/pdf")
+                self.set_header('Content-Disposition', saved_file_name)
+                self.write(pdf)
+            except Exception, e:
+                msg = "The File is not written using markdown!"
+                self.render("error.html",msg=msg)
+
+def _path(name):
+    folder = os.path.join(os.path.dirname(__file__), 'files')
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    return '%s/%s' % (folder, name)
+
+def save(name, data):
+    f = None
+    try:
+        f = open(_path(name), 'wb')
+        f.write(data)
+    except:
+        pass
+    finally:
+        if f: f.close()
+    return './files/' + name
 
 
 settings = {
